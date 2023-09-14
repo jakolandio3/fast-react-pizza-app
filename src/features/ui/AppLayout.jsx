9e749b10bd217e9496b0e4eef9cc80524caa67c2
Jakob Douglas <jakobdouglas1@gmail.com>
2023-09-14 16:24:22 +0930

    style: added new icons and tailwind classes for stylised components, also added more responsive mobile first design

--- src/features/ui/AppLayout.jsx
+++ src/features/ui/AppLayout.jsx
@@ -7,7 +7,7 @@ function AppLayout() {
   const navigation = useNavigation();
   const isLoading = navigation.state === "loading";
   return (
-    <div className="grid h-[100vh] grid-rows-[auto_1fr_auto]">
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader />}
 
       <Header />
